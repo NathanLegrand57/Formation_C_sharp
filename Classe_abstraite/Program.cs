05a9e4ac9f4d4f4bd8398b7f0436313305cfce6c
Nathan <115118374+Nathan4457@users.noreply.github.com>
2023-06-09 12:02:42 +0200

    Add files via upload

---
+++ Classe_abstraite/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+abstract class Animaux
+{
+    public void Terre()
+    {
+        Console.WriteLine("Je suis sur la Terre");
+    }
+    public abstract void Bruit();
+}
+class Chat : Animaux
+{
+    public override void Bruit() => Console.WriteLine("Miaouw");
+}
+
+class Chien : Animaux
+{
+    public override void Bruit() => Console.WriteLine("Ouaf");
+}
+
+class program
+{
+    public static void Main(string []args)
+    {
+        Chat c = new Chat();
+        Chien ch = new Chien();
+
+        c.Bruit();
+        ch.Bruit();
+
+        c.Terre();
+        ch.Terre();
+    }
+}
+
---
+++ Date/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+// DateTime dt = new DateTime();
+// DateTimeOffset dto = new DateTimeOffset(dt);
+DateTime thisHour = DateTime.Now;
+// while (thisHour != null)
+// {
+// thisHour = DateTime.Now;
+// Thread.Sleep(1000);
+// Console.WriteLine(thisHour);
+// }
+DateTime dt = new DateTime(2022, 8, 13, 10, 30, 40);
+TimeZoneInfo tzi = TimeZoneInfo.Local;
+
+DateTime finalDate = TimeZoneInfo.ConvertTime(dt, tzi);
+Console.WriteLine(finalDate);
+
+// Console.WriteLine(tzi);
+
+// DateTime dt = new DateTime();
+// dt.AddDays(3);
+// Console.WriteLine(dt.AddDays(3));
+// Console.WriteLine(DateTime.DaysInMonth(2023, 2));
+// string s = "2022-5-28";
+// if(DateTime.TryParse(s, out dt))
+// {
+//     Console.WriteLine(dt);
+// }
